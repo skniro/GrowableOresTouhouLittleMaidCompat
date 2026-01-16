65772848c066416c3cbc55b41588d9a72bf0c0eb
skniro <46392259+skniro@users.noreply.github.com>
2026-01-16 20:31:58 +0800

    1.20.1

--- Forge/src/main/java/com/skniro/growable_ores_touhou_little_maid_compat/GrowableOresExtension.java
+++ Forge/src/main/java/com/skniro/growable_ores_touhou_little_maid_compat/GrowableOresExtension.java
@@ -11,10 +11,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(GrowableOresExtension.MODID)
+@Mod(GrowableOresExtension.MOD_ID)
 public class GrowableOresExtension {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "growable_ores_touhou_little_maid_compat";
+    public static final String MOD_ID = "growable_ores_touhou_little_maid_compat";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
